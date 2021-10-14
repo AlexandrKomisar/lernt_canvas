db8c0febf8f2b665cba8744047760e2b369072ba
AlexandrKomisar <82936052+AlexandrKomisar@users.noreply.github.com>
2021-10-14 14:16:17 +0300

    ctx

--- js/canvas.js
+++ js/canvas.js
@@ -1,5 +1,34 @@
-var canvas = document.getElementById("canvas");
-var ctx = canvas.getContext("2d");
+// var canvas = document.getElementById("canvas");
+// var ctx = canvas.getContext("2d");
 
-ctx.fillStyle = "green";
-ctx.fillRect(10, 10, 100, 100)
+// ctx.fillStyle = "green";
+// ctx.fillRect(50, 50, 400, 400)
+
+function draw() {
+    var canvas = document.getElementById("canvas");
+    if (canvas.getContext) {
+      var ctx = canvas.getContext("2d");
+
+      ctx.fillStyle = "rgb(200,0,0.5)";
+      ctx.fillRect (0, 0, 150, 150);
+
+      ctx.fillStyle = "rgba(0, 0, 200, 0.5)";
+      ctx.fillRect (150, 150, 150, 150);
+
+      ctx.fillStyle = "rgb(200,0,0.5)";
+      ctx.fillRect (300, 0, 150, 150);
+
+      ctx.fillStyle = "rgba(0, 0, 200, 0.5)";
+      ctx.fillRect (450, 150, 150, 150);
+
+      ctx.fillStyle = "rgb(200,0,0.5)";
+      ctx.fillRect (600, 0, 150, 150);
+
+      ctx.fillStyle = "rgba(0, 0, 200, 0.5)";
+      ctx.fillRect (750, 150, 150, 150);
+      
+    };
+    
+
+    
+}
