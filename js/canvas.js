073b65126b85ec31b9a1c8270ba646ab60d3f60e
AlexandrKomisar <82936052+AlexandrKomisar@users.noreply.github.com>
2021-12-26 15:55:59 +0200

    001

--- js/canvas.js
+++ js/canvas.js
@@ -9,24 +9,8 @@ function draw() {
     if (canvas.getContext) {
       var ctx = canvas.getContext("2d");
 
-      ctx.fillStyle = "rgb(200,0,0.5)";
-      ctx.fillRect (0, 0, 150, 150);
-
-      ctx.fillStyle = "rgba(0, 0, 200, 0.5)";
+      ctx.fillStyle = "rgba(0, 0, 200, 0.8)";
       ctx.fillRect (150, 150, 150, 150);
-
-      ctx.fillStyle = "rgb(200,0,0.5)";
-      ctx.fillRect (300, 0, 150, 150);
-
-      ctx.fillStyle = "rgba(0, 0, 200, 0.5)";
-      ctx.fillRect (450, 150, 150, 150);
-
-      ctx.fillStyle = "rgb(200,0,0.5)";
-      ctx.fillRect (600, 0, 150, 150);
-
-      ctx.fillStyle = "rgba(0, 0, 200, 0.5)";
-      ctx.fillRect (750, 150, 150, 150);
-      
     };
     
 
